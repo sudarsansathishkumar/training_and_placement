32e755d47ed79b3aadd3f6ba3f3619e5c6aca4e4
sudarsansathishkumar <138559053+sudarsansathishkumar@users.noreply.github.com>
2024-01-20 15:55:52 +0530

    Update d2_3.c

--- day_2/d2_3.c
+++ day_2/d2_3.c
@@ -8,10 +8,7 @@ int main() {
     printf("Enter a string: ");
     gets(str);
     while(str[l] != '\0') l++;
-    for(i = l - 1; i >= 0 ; i--) {
-        str[l++] = str[i];
-        printf("%d", i);
-    }
+    for(i = l - 1; i >= 0 ; i--) str[l++] = str[i];
     str[l] = '\0';
     printf("The palindrome string is: %s", str);
     return 0;
